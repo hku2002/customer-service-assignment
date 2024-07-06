5a42b1b40126656abe9f96e32d2a7e9fc3460a9e
hku2002 <hku2002@hanmail.net>
2024-07-06 23:09:37 +0900

    feat: spring project 추가

---
+++ src/main/java/com/connie/customer/CustomerServiceAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.connie.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/connie/customer/CustomerServiceAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connie.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
