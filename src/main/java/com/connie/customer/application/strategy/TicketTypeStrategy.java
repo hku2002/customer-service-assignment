7762a43b8fe16d00a87c38e23566fbe97d7ff008
hku2002 <hku2002@hanmail.net>
2024-07-07 19:37:50 +0900

    refactor: 주석 추가

--- src/main/java/com/connie/customer/application/strategy/TicketTypeStrategy.java
+++ src/main/java/com/connie/customer/application/strategy/TicketTypeStrategy.java
@@ -5,8 +5,17 @@ import com.connie.customer.domain.enums.TicketType;
 
 public interface TicketTypeStrategy {
 
+    /**
+     * 티켓 타입별 전략이 맞는지 확인하는 메소드
+     * @param type 티켓 타입
+     * @return boolean
+     */
     boolean isValidStrategy(TicketType type);
 
+    /**
+     * 타입별 티켓 생성시 필요한 필드가 존재하는지 확인하는 메소드
+     * @param request 요청 받은 필드
+     */
     void checkTypeField(CreateTicketRequest request);
 
 }
