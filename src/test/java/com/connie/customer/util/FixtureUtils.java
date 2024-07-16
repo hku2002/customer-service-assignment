2fe1d066533aeb344ff8a1c1f914f8bc8efe3469
hku2002 <hku2002@hanmail.net>
2024-07-16 22:32:31 +0900

    fix: 오류 수정

--- src/test/java/com/connie/customer/util/FixtureUtils.java
+++ src/test/java/com/connie/customer/util/FixtureUtils.java
@@ -1,24 +1,13 @@
 package com.connie.customer.util;
 
 import com.navercorp.fixturemonkey.FixtureMonkey;
-import com.navercorp.fixturemonkey.api.introspector.BeanArbitraryIntrospector;
-import com.navercorp.fixturemonkey.api.introspector.BuilderArbitraryIntrospector;
-import com.navercorp.fixturemonkey.api.introspector.FailoverIntrospector;
-import com.navercorp.fixturemonkey.api.introspector.FieldReflectionArbitraryIntrospector;
-
-import java.util.List;
+import com.navercorp.fixturemonkey.api.introspector.ConstructorPropertiesArbitraryIntrospector;
 
 public class FixtureUtils {
 
     public static FixtureMonkey getInstance() {
         return FixtureMonkey.builder()
-                .objectIntrospector(new FailoverIntrospector(
-                        List.of(
-                                FieldReflectionArbitraryIntrospector.INSTANCE,
-                                BeanArbitraryIntrospector.INSTANCE,
-                                BuilderArbitraryIntrospector.INSTANCE
-                        )
-                ))
+                .objectIntrospector(ConstructorPropertiesArbitraryIntrospector.INSTANCE)
                 .defaultNotNull(true)
                 .build();
     }
