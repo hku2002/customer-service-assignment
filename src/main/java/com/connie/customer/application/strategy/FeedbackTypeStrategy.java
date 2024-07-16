cade9822d4472de1a6df8051212a45a77611db9d
hku2002 <hku2002@hanmail.net>
2024-07-16 21:49:16 +0900

    fix: 잘못된 로직 제거

--- src/main/java/com/connie/customer/application/strategy/FeedbackTypeStrategy.java
+++ src/main/java/com/connie/customer/application/strategy/FeedbackTypeStrategy.java
@@ -3,7 +3,6 @@ package com.connie.customer.application.strategy;
 import com.connie.customer.api.dto.request.CreateTicketRequest;
 import com.connie.customer.domain.enums.TicketType;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 import static com.connie.customer.domain.enums.TicketType.FEEDBACK;
 
@@ -18,9 +17,6 @@ public class FeedbackTypeStrategy implements TicketTypeStrategy {
     public void checkTypeField(CreateTicketRequest request) {
         if (!FEEDBACK.equals(request.type())) {
             throw new IllegalArgumentException("피드백 문의 타입이 아닙니다.");
-        }
-        if (StringUtils.hasText(request.phoneNumber()) && request.userId() != null) {
-            throw new IllegalArgumentException("피드백 문의 타입의 필수값이 없습니다.");
         }
     }
 }
